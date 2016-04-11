a18350293c60ebb75caa507eb1b38adbda6c7c16
gordonszhang <gordonszhang@outlook.com>
2016-04-10 21:15:48 -0400

    Added guard and round bits

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ string toBinary(int value, int length) {
 
 int main() {
   int x, y = -1;
-  int x_sign, x_exp, x_frac;
-  int y_sign, y_exp, y_frac;
+  int x_sign, x_exp, x_frac, x_gr;
+  int y_sign, y_exp, y_frac, y_gr;
   float x_val, y_val;
 
   while(x < 0 || x > 255 || y < 0 || y > 255) {
@@ -35,22 +35,25 @@ int main() {
   x_sign = (x >> 7) & 1;
   x_exp = (x >> 4) & 0x7;
   x_frac = x & 0xf;
+  x_gr = 0;
 
   y_sign = (y >> 7) & 1;
   y_exp = (y >> 4) & 0x7;
   y_frac = y & 0xf;
+  y_gr = 0;
+
 
   cout << "internal rep of first value: " << setw(3) << 1 << "." << toBinary(x_frac, 4)
-       << " x 2^(";
+       << " " << toBinary(x_gr, 2) << " x 2^(";
   if((x_exp - 4) > 0) cout << "+";
   cout << x_exp - 4 << ")" << endl;
 
   cout << "internal rep of second value: " << setw(2) << 1 << "." << toBinary(y_frac, 4)
-       << " x 2^(";
+       << " " << toBinary(y_gr, 2) << " x 2^(";
   if((y_exp - 4) > 0) cout << "+";
   cout << y_exp - 4 << ")" << endl;
 
-  cout << "hidden bit---------------------^ ffff" << endl;
+  cout << "hidden bit---------------------^ ffff gr" << endl;
   cout << "4-bit fraction-------------------^^^^" << endl << endl;
 
   x_val = (1 + x_frac / 16.0) * pow(2, x_exp - 4);
@@ -62,6 +65,7 @@ int main() {
   cout << "second value is: " << setw(1) << (!y_sign ? "+" : "-") << setw(6)
        << setfill('0') << left << 1 + y_frac / 16.0 << " x 2^("
        << (((y_exp - 4) > 0) ? "+" : "") << y_exp - 4 << ") = " << y_val << endl;
+
 
   return 0;
 }
