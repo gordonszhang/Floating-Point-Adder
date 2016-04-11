65d1f737210bd0fdf36d16f3495ed4dd52129fc3
gordonszhang <gordonszhang@outlook.com>
2016-04-10 21:42:38 -0400

    Added hidden bit

--- main.cpp
+++ main.cpp
@@ -18,8 +18,8 @@ string toBinary(int value, int length) {
 
 int main() {
   int x, y = -1;
-  int x_sign, x_exp, x_frac, x_gr;
-  int y_sign, y_exp, y_frac, y_gr;
+  int x_sign, x_hide, x_exp, x_frac, x_gr;
+  int y_sign, y_hide, y_exp, y_frac, y_gr;
   float x_val, y_val;
 
   while(x < 0 || x > 255 || y < 0 || y > 255) {
@@ -33,22 +33,24 @@ int main() {
   cout << "encoding of second value: " << setw(13) << toBinary(y, 8) << endl << endl;
 
   x_sign = (x >> 7) & 1;
+  x_hide = 1;
   x_exp = (x >> 4) & 0x7;
   x_frac = x & 0xf;
   x_gr = 0;
 
   y_sign = (y >> 7) & 1;
+  y_hide = 1;
   y_exp = (y >> 4) & 0x7;
   y_frac = y & 0xf;
   y_gr = 0;
 
 
-  cout << "internal rep of first value: " << setw(3) << 1 << "." << toBinary(x_frac, 4)
+  cout << "internal rep of first value: " << setw(3) << x_hide << "." << toBinary(x_frac, 4)
        << " " << toBinary(x_gr, 2) << " x 2^(";
   if((x_exp - 4) > 0) cout << "+";
   cout << x_exp - 4 << ")" << endl;
 
-  cout << "internal rep of second value: " << setw(2) << 1 << "." << toBinary(y_frac, 4)
+  cout << "internal rep of second value: " << setw(2) << y_hide << "." << toBinary(y_frac, 4)
        << " " << toBinary(y_gr, 2) << " x 2^(";
   if((y_exp - 4) > 0) cout << "+";
   cout << y_exp - 4 << ")" << endl;
@@ -56,14 +58,14 @@ int main() {
   cout << "hidden bit---------------------^ ffff gr" << endl;
   cout << "4-bit fraction-------------------^^^^" << endl << endl;
 
-  x_val = (1 + x_frac / 16.0) * pow(2, x_exp - 4);
+  x_val = (x_hide + x_frac / 16.0) * pow(2, x_exp - 4);
   cout << "first value is: " << setw(2) << (!x_sign ? "+" : "-") << setw(6)
-       << setfill('0') << left << 1 + x_frac / 16.0 << " x 2^("
+       << setfill('0') << left << x_hide + x_frac / 16.0 << " x 2^("
        << (((x_exp - 4) > 0) ? "+" : "") << x_exp - 4 << ") = " << x_val << endl;
 
-  y_val = (1 + y_frac / 16.0) * pow(2, y_exp - 4);
+  y_val = (y_hide + y_frac / 16.0) * pow(2, y_exp - 4);
   cout << "second value is: " << setw(1) << (!y_sign ? "+" : "-") << setw(6)
-       << setfill('0') << left << 1 + y_frac / 16.0 << " x 2^("
+       << setfill('0') << left << y_hide + y_frac / 16.0 << " x 2^("
        << (((y_exp - 4) > 0) ? "+" : "") << y_exp - 4 << ") = " << y_val << endl;
 
   cout << endl << "addition of the values: " << endl;
